c775f510f8a61c8c68a0df484179de00632fe21b
unknown <stasuss@gmail.com>
2016-01-02 21:24:41 +0300

    initial commit

---
+++ DigitalAnalyzer/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace DigitalAnalyzer {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series16 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ctrlList = new System.Windows.Forms.ListBox();
+            this.dataList = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.resetChk = new System.Windows.Forms.CheckBox();
+            this.load = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "save data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(499, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "4160 microseconds each";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX.LabelAutoFitStyle = System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.DecreaseFont;
+            chartArea1.AxisX.MajorGrid.Interval = 1D;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.DarkOliveGreen;
+            chartArea1.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisX.MaximumAutoSize = 1F;
+            chartArea1.AxisX.ScaleView.MinSize = 0D;
+            chartArea1.AxisX.ScaleView.Size = 100D;
+            chartArea1.AxisX.ScrollBar.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.AxisY.Interval = 1D;
+            chartArea1.AxisY.MajorGrid.Interval = 1D;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            chartArea1.AxisY.Maximum = 33D;
+            chartArea1.AxisY.MaximumAutoSize = 1F;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.ScaleView.MinSize = 33D;
+            chartArea1.AxisY.ScaleView.Size = 33D;
+            chartArea1.AxisY.ScaleView.Zoomable = false;
+            chartArea1.BackColor = System.Drawing.Color.Black;
+            chartArea1.CursorX.IsUserEnabled = true;
+            chartArea1.CursorX.IsUserSelectionEnabled = true;
+            chartArea1.CursorX.SelectionColor = System.Drawing.Color.Gainsboro;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(174, 34);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series1.Legend = "Legend1";
+            series1.Name = "green1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series2.Legend = "Legend1";
+            series2.Name = "green2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series3.Legend = "Legend1";
+            series3.Name = "yellow1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series4.Legend = "Legend1";
+            series4.Name = "yellow2";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            series5.Legend = "Legend1";
+            series5.Name = "purple1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            series6.Legend = "Legend1";
+            series6.Name = "purple2";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            series7.Legend = "Legend1";
+            series7.Name = "purple3";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            series8.Legend = "Legend1";
+            series8.Name = "purple4";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series9.Legend = "Legend1";
+            series9.Name = "data0";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series10.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series10.Legend = "Legend1";
+            series10.Name = "data1";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series11.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series11.Legend = "Legend1";
+            series11.Name = "data2";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series12.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series12.Legend = "Legend1";
+            series12.Name = "data3";
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series13.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series13.Legend = "Legend1";
+            series13.Name = "data4";
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series14.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series14.Legend = "Legend1";
+            series14.Name = "data5";
+            series15.ChartArea = "ChartArea1";
+            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series15.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series15.Legend = "Legend1";
+            series15.Name = "data6";
+            series16.ChartArea = "ChartArea1";
+            series16.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series16.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series16.Legend = "Legend1";
+            series16.Name = "data7";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Series.Add(series13);
+            this.chart1.Series.Add(series14);
+            this.chart1.Series.Add(series15);
+            this.chart1.Series.Add(series16);
+            this.chart1.Size = new System.Drawing.Size(1011, 420);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            this.chart1.CursorPositionChanged += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.CursorEventArgs>(this.chart1_CursorPositionChanged);
+            this.chart1.MouseEnter += new System.EventHandler(this.chart1_MouseEnter);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "save control";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // ctrlList
+            // 
+            this.ctrlList.FormattingEnabled = true;
+            this.ctrlList.Location = new System.Drawing.Point(12, 34);
+            this.ctrlList.Name = "ctrlList";
+            this.ctrlList.Size = new System.Drawing.Size(75, 420);
+            this.ctrlList.TabIndex = 9;
+            // 
+            // dataList
+            // 
+            this.dataList.FormattingEnabled = true;
+            this.dataList.Location = new System.Drawing.Point(93, 34);
+            this.dataList.Name = "dataList";
+            this.dataList.Size = new System.Drawing.Size(75, 420);
+            this.dataList.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resetChk
+            // 
+            this.resetChk.AutoSize = true;
+            this.resetChk.Location = new System.Drawing.Point(336, 11);
+            this.resetChk.Name = "resetChk";
+            this.resetChk.Size = new System.Drawing.Size(106, 17);
+            this.resetChk.TabIndex = 12;
+            this.resetChk.Text = "reset on connect";
+            this.resetChk.UseVisualStyleBackColor = true;
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(1110, 7);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(75, 23);
+            this.load.TabIndex = 13;
+            this.load.Text = "load";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(1029, 7);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 14;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(790, 7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "fill lists";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 462);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.resetChk);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataList);
+            this.Controls.Add(this.ctrlList);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox ctrlList;
+        private System.Windows.Forms.ListBox dataList;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox resetChk;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ DigitalAnalyzer/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DigitalAnalyzer {
+    public partial class Form1:Form {
+        private static SerialPort com;
+        private int prevX = 0;
+        private delegate void GetTextDeleg(List<string> strings);
+        private delegate void GetStringDeleg(string data);
+
+        public Form1() {
+            InitializeComponent();
+            chart1.MouseWheel += new MouseEventHandler(chart1_MouseWheel);
+        }
+
+        private void chart1_MouseWheel(object sender, MouseEventArgs e) {
+            try {
+                if (e.Delta < 0) {
+                    //chart1.ChartAreas[0].AxisX.ScaleView.ZoomReset();
+                    //chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+
+                    double xMin = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                    double xMax = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+                    double yMin = chart1.ChartAreas[0].AxisY.ScaleView.ViewMinimum;
+                    double yMax = chart1.ChartAreas[0].AxisY.ScaleView.ViewMaximum;
+
+                    double posXStart = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin);
+                    double posXFinish = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin);
+                    double posYStart = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) - (yMax - yMin);
+                    double posYFinish = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) + (yMax - yMin);
+
+                    chart1.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+                    chart1.ChartAreas[0].AxisY.ScaleView.Zoom(posYStart, posYFinish);
+                }
+
+                if (e.Delta > 0) {
+                    double xMin = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                    double xMax = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+                    double yMin = chart1.ChartAreas[0].AxisY.ScaleView.ViewMinimum;
+                    double yMax = chart1.ChartAreas[0].AxisY.ScaleView.ViewMaximum;
+
+                    double posXStart = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                    double posXFinish = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+                    double posYStart = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 4;
+                    double posYFinish = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 4;
+
+                    chart1.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+                    chart1.ChartAreas[0].AxisY.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            } catch { }
+
+            Debug.WriteLine("mouse");
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            try { com.Close(); } catch { }
+
+            try {
+                com = new SerialPort("COM3", 19200);
+                com.DataReceived += new SerialDataReceivedEventHandler(gotSomething);
+                com.DtrEnable = resetChk.Checked;
+                com.Open();
+            } catch { MessageBox.Show("something went wrong!"); }
+        }
+
+        void gotSomething(object sender, SerialDataReceivedEventArgs e) {
+            String input = String.Empty;
+            //input = com.ReadLine();
+
+            //BeginInvoke(new GetStringDeleg(processInputString), new object[] { input });
+
+            int bytes = com.BytesToRead;
+            byte[] buffer = new byte[bytes];
+            com.Read(buffer, 0, bytes);
+
+            if (buffer != null) bytesToStrings(buffer);
+        }
+
+        void bytesToStrings(byte[] bytes) {
+            List<string> strings = new List<string>();
+
+            string temp = String.Empty;
+
+            for (int i = 0; i < bytes.Length; i++) {
+                if ((char)bytes[i] != '\r') {
+                    temp += (char)bytes[i];
+                } else {
+                    if (temp != String.Empty) {
+                        strings.Add(temp);
+                        temp = String.Empty;
+                    }
+                }
+            }
+
+            BeginInvoke(new GetTextDeleg(processInput), new object[] { strings });
+        }
+
+        private void processInput(List<string> strings) {
+            for (int s = 0; s < strings.Count; s++) {
+                processInputString(strings[s]);
+            }
+
+            /*
+            if (counter++ % 2 == 0) {
+                processInputString("\n01010101");
+            } else {
+                processInputString("\n10101010");
+            }*/
+        }
+
+        private void processInputString(string data) {
+            string bin = Convert.ToString(Int32.Parse(data), 2).PadLeft(16, '0');
+
+            chart1.Series.SuspendUpdates();
+
+            int offset = 1;
+
+            //if (data.Length < 8) return;
+
+            for (int i = 15; i >= 0; i--) {
+                //if (bin[i] == '\n' || data[i] == ';') continue;
+
+                double y = offset + (bin[i] == '1' ? 1 : 0);
+
+                chart1.Series[i].Points.AddXY(prevX, y);
+                chart1.Series[i].Points.AddXY(prevX + 1, y);
+                offset += 2;
+            }
+
+            prevX++;
+
+            chart1.Series.ResumeUpdates();
+
+            chart1.ChartAreas[0].AxisX.ScaleView.Scroll(chart1.ChartAreas[0].AxisX.Maximum);
+
+            ctrlList.Items.Add(bin.Substring(0, 8));
+            dataList.Items.Add(bin.Substring(8, 8));
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            try { com.Close(); } catch { }
+            com.DataReceived -= gotSomething;
+        }
+
+        private void chart1_MouseEnter(object sender, EventArgs e) {
+            chart1.Focus();
+        }
+
+        private void chart1_CursorPositionChanged(object sender, CursorEventArgs e) {
+            ctrlList.SelectedIndex = (int)e.NewPosition;
+            dataList.SelectedIndex = (int)e.NewPosition;
+        }
+
+        private void load_Click(object sender, EventArgs e) {
+            OpenFileDialog loadFile = new OpenFileDialog();
+            loadFile.Title = "Load data";
+            loadFile.ShowDialog();
+
+            if (loadFile.FileName != "") {
+                chart1.Serializer.Load(loadFile.FileName);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e) {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "Save data";
+            saveFile.ShowDialog();
+
+            if (saveFile.FileName != "") {
+                chart1.Serializer.Save(saveFile.FileName);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            ctrlList.Items.Clear();
+            dataList.Items.Clear();
+
+            string bin;
+            int total = chart1.Series.Count;
+            int y = 0;
+
+            for (double x = 0; x < chart1.ChartAreas[0].AxisX.Maximum; x++) {
+                bin = String.Empty;
+                for (int s = 0; s < total; s++) {
+                    y = total * 2 - s * 2;
+                    bin += chart1.Series[s].Points.Where(point => point.XValue == x).First().YValues[0] == y ? 1 : 0;
+                }
+
+                ctrlList.Items.Add(bin.Substring(0, 8));
+                dataList.Items.Add(bin.Substring(8, 8));
+            }
+        }
+    }
+}
